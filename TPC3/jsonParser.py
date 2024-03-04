5dadc6808951217e1d0990d0fd690ad5ae28b39e
André Alves <nseiporamor@gmail.com>
2024-03-04 16:38:27 +0000

    Script done

---
+++ TPC3/jsonParser.py
@@ -0,0 +1,37 @@
+import json
+
+f = open("mapa.json", encoding='utf-8')
+
+bd = json.load(f)
+
+f.close()
+
+ttl = ""
+for cidade in bd['cidades']:
+    ttl += f"""
+###  http://www.semanticweb.org/utilizador/ontologies/2024/1/mapa-virtual#{cidade["id"]}
+:{cidade["id"]} rdf:type owl:NamedIndividual ,
+                :cidade ;
+         :descricao "{cidade["descrição"]}" ;
+         :distrito "{cidade["distrito"]}" ;
+         :id "{cidade["id"]}" ;
+         :nome "{cidade["nome"]}" ;
+         :populacao {cidade["população"]} .
+    """
+
+for ligacao in bd['ligacoes']:
+    ttl += f"""
+###  http://www.semanticweb.org/utilizador/ontologies/2024/1/mapa-virtual#{ligacao["id"]}
+:{ligacao["id"]} rdf:type owl:NamedIndividual ,
+                :ligacao ;
+         :destino :{ligacao["destino"]} ;
+         :origem :{ligacao["origem"]} ;
+         :distancia {ligacao["distância"]} ;
+         :id "{ligacao["id"]}" .
+    """
+
+f = open("out.ttl", "w", encoding='utf-8')
+
+f.write(ttl)
+
+f.close()
